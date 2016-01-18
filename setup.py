d12536cbc0b960c1a9d9df0625c2e877413cfdcd
Sumanth <sumansn@thoughtworks.com>
2016-01-18 17:32:14 -0600

    Fix parse_requirements to work with older pip

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-install_reqs = parse_requirements('requirements.txt')
+install_reqs = parse_requirements('requirements.txt', session=False)
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name='sonic_client',
