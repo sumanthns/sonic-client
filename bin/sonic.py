a12ad56d6f752300ee36c1e30a0cb8011b7e5408
Sumanth <sumansn@thoughtworks.com>
2016-01-18 19:18:57 -0600

    Changed daemon library to python-daemon

--- bin/sonic.py
+++ bin/sonic.py
@@ -1,25 +1,43 @@
 import logging
 
-from daemonize import Daemonize
+from daemon import runner
+import time
 from sonic_client.sonic_client import SonicClient
 
-pid = "tmp/sonic.pid"
+pid = "/var/run/sonic_client/sonic.pid"
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.propagate = False
-fh = logging.FileHandler("tmp/sonic_client.log", "w")
+fh = logging.FileHandler("/vat/log/sonic_client/sonic_client.log", "w")
 fh.setLevel(logging.DEBUG)
 logger.addHandler(fh)
 keep_fds = [fh.stream.fileno()]
 
+class App():
 
-def app():
-    SonicClient(logger).run()
+    def __init__(self):
+        self.stdin_path = '/dev/null'
+        self.stdout_path = '/dev/tty'
+        self.stderr_path = '/dev/tty'
+        self.pidfile_path =  pid
+        self.pidfile_timeout = 5
+
+    def run(self):
+        while True:
+            SonicClient(logger).run()
+            logger.debug("Debug message")
+            logger.info("Info message")
+            logger.warn("Warning message")
+            logger.error("Error message")
+            time.sleep(10)
+
+app = App()
 
 def main():
     logger.debug("Starting sonic client")
-    daemon = Daemonize(app="sonic_client", pid=pid, action=app, keep_fds=keep_fds, logger=logger)
-    daemon.start()
+    daemon_runner = runner.DaemonRunner(app)
+    daemon_runner.daemon_context.files_preserve=[fh.stream]
+    daemon_runner.do_action()
 
 if __name__ == "__main__":
     main()
