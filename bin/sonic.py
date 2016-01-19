668433c62804ad16e168d4e650f28be481ded0af
Sumanth <sumansn@thoughtworks.com>
2016-01-18 18:56:39 -0600

    fix daemon

--- bin/sonic.py
+++ bin/sonic.py
@@ -3,11 +3,11 @@ import logging
 from daemonize import Daemonize
 from sonic_client.sonic_client import SonicClient
 
-pid = "/tmp/sonic.pid"
+pid = "tmp/sonic.pid"
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.propagate = False
-fh = logging.FileHandler("/var/log/sonic_client/sonic_client.log", "w")
+fh = logging.FileHandler("tmp/sonic_client.log", "w")
 fh.setLevel(logging.DEBUG)
 logger.addHandler(fh)
 keep_fds = [fh.stream.fileno()]
@@ -18,7 +18,7 @@ def app():
 
 def main():
     logger.debug("Starting sonic client")
-    daemon = Daemonize(app="sonic_client", pid=pid, action=app, keep_fds=keep_fds)
+    daemon = Daemonize(app="sonic_client", pid=pid, action=app, keep_fds=keep_fds, logger=logger)
     daemon.start()
 
 if __name__ == "__main__":
