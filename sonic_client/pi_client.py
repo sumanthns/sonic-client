089f216f5ba8ca41134b9923bc6d3c77d64c077a
Sumanth <sumansn@thoughtworks.com>
2016-01-18 22:08:09 -0600

    GPIO set mode to BCM

--- sonic_client/pi_client.py
+++ sonic_client/pi_client.py
@@ -1,6 +1,10 @@
 import RPi.GPIO as GPIO
 
 class PiClient(object):
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+
     def output(self, led, value):
         GPIO.setup(led, GPIO.OUT)
         GPIO.output(led, value)
